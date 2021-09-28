0ea436b0a9efb1b65969e6d9579b8671e8949436
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-28 16:22:27 -0300

    alteracoes desafio 1

--- src/desafio.java
+++ src/desafio.java
@@ -2,13 +2,13 @@ public class desafio {
     public static void main(String[] args) {
         titulo();
 
-        int resultadoSoma1 = soma(10, 20);
+        int resultadoSoma1 = soma(33, 20);
         saida_int("Soma 1", resultadoSoma1);
-        int resultadoSoma2 = soma(20, 30);
+        int resultadoSoma2 = soma(20, 33);
         saida_int("Soma 2", resultadoSoma2);
-        int resultadoSoma3 = soma(30, 40);
+        int resultadoSoma3 = soma(33, 40);
         saida_int("Soma 3", resultadoSoma3);
-        int resultadoSoma4 = soma(40, 50);
+        int resultadoSoma4 = soma(40, 53);
         saida_int("Soma 4", resultadoSoma4);
 
         double resultadoMedia = media(resultadoSoma1, resultadoSoma2, resultadoSoma3, resultadoSoma4);
@@ -25,7 +25,7 @@ public class desafio {
     }
 
     static double media(int a, int b, int c, int d) {
-        double calculo = (a + b + c + d) / 4;
+        double calculo = (a + b + c + d) / 4.0;
         return calculo;
     }
 
